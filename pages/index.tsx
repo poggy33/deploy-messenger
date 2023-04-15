d982db854c33eb3f73527456d6bb017c88a51ea4
poggy33 <33poggy33@gmail.com>
2023-04-15 20:47:36 +0300

    first

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Form from "@/components/Form";
+import PostFeed from "@/components/posts/PostFeed";
+
+
+export default function Home() {
+  return (
+    <>
+    <Header showBackArrow label="Home"/>
+    <Form placeholder="What's happening?" />
+    <PostFeed/>
+    </>
+  )
+}
